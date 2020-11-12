2ade9a76af6395ec047269ffd8b6182596ca3e78
John Hare <mostly_me@hotmail.com>
2020-11-12 16:48:16 +0000

    Constants moved to config class
    Change-Id: I30913ef619319f68019b900e7a06cba8f30850d3

--- src/main/java/uk/co/jfh/romans/app/Romans.java
+++ src/main/java/uk/co/jfh/romans/app/Romans.java
@@ -7,33 +7,23 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.Map;
+
+import uk.co.jfh.romans.conf.Mappings;
 
 public class Romans {
 
     private static InputStream inputStream = System.in;
     private static OutputStream outputStream = System.out;
 
-    private static final Map<Integer, String> UNITS = Map.of(1, "I", 2, "II", 3, "III", 4, "IV", 5, "V", 6, "VI", 7,
-            "VII", 8, "VIII", 9, "IX");
-
-    private static final Map<Integer, String> TENS = Map.of(10, "X", 20, "XX", 30, "XXX", 40, "XL", 50, "L", 60, "LX",
-            70, "LXX", 80, "LXXX", 90, "XC");
-
-    private static final Map<Integer, String> HUNDREDS = Map.of(100, "C", 200, "CC", 300, "CCC", 400, "CD", 500, "D",
-            600, "DC", 700, "DCC", 800, "DCCC", 900, "CM");
-
-    private static final Map<Integer, String> THOUSANDS = Map.of(1000, "M", 2000, "MM", 3000, "MMM");
-
     public static void main(String[] args) {
-        String convertedValue;
+        String response;
         String input = getUserInput();
         if (validateInput(input)) {
-            convertedValue = convertInput(input);
+            response = convertInput(input);
         } else {
-            convertedValue = "Invalid input";
+            response = "Invalid input";
         }
-        outputConvertedValue(convertedValue);
+        outputConvertedValue(response);
     }
 
     public static void outputConvertedValue(String convertedValue) {
@@ -52,16 +42,16 @@ public class Romans {
         Integer units = (value - thousands - hundreds - tens);
         StringBuilder result = new StringBuilder();
         if (thousands > 0) {
-            result.append(THOUSANDS.get(thousands));
+            result.append(Mappings.THOUSANDS.get(thousands));
         }
         if (hundreds > 0) {
-            result.append(HUNDREDS.get(hundreds));
+            result.append(Mappings.HUNDREDS.get(hundreds));
         }
         if (tens > 0) {
-            result.append(TENS.get(tens));
+            result.append(Mappings.TENS.get(tens));
         }
         if (units > 0) {
-            result.append(UNITS.get(units));
+            result.append(Mappings.UNITS.get(units));
         }
         return result.toString();
     }
---
+++ src/main/java/uk/co/jfh/romans/conf/package-info.java
@@ -0,0 +1,1 @@
+package uk.co.jfh.romans.conf;
